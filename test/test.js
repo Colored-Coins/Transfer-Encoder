0e71128e129275095b0c90aaae1b5fab79c1c0b0
eliran zach <thehobbit85@gmail.com>
2015-10-15 16:23:38 +0300

    Fixed typo

--- test/test.js
+++ test/test.js
@@ -29,32 +29,32 @@ describe('BALZ Decoding', function () {
     data.torrentHash = torrentHash
 
     data.payments = []
-    data.payments.push({skip: false, range: false, precent: true, output: 12, amountOfUnits: 3213213})
+    data.payments.push({skip: false, range: false, percent: true, output: 12, amountOfUnits: 3213213})
     var result = ccEncoding.encode(data, 40)
     console.log(result.codeBuffer.toString('hex'), result.leftover)
     console.log(ccEncoding.decode(result.codeBuffer))
 
-    data.payments.push({skip: false, range: false, precent: true, output: 1, amountOfUnits: 321321321})
+    data.payments.push({skip: false, range: false, percent: true, output: 1, amountOfUnits: 321321321})
     result = ccEncoding.encode(data, 40)
     console.log(result.codeBuffer.toString('hex'), result.leftover)
     console.log(ccEncoding.decode(result.codeBuffer))
 
-    data.payments.push({skip: true, range: true, precent: true, output: 10, amountOfUnits: 1})
+    data.payments.push({skip: true, range: true, percent: true, output: 10, amountOfUnits: 1})
     result = ccEncoding.encode(data, 40)
     console.log(result.codeBuffer.toString('hex'), result.leftover)
     console.log(ccEncoding.decode(result.codeBuffer))
 
-    data.payments.push({skip: false, range: false, precent: true, output: 20, amountOfUnits: 100000021000})
+    data.payments.push({skip: false, range: false, percent: true, output: 20, amountOfUnits: 100000021000})
     result = ccEncoding.encode(data, 40)
     console.log(result.codeBuffer.toString('hex'), result.leftover)
     console.log(ccEncoding.decode(result.codeBuffer))
 
-    data.payments.push({skip: false, range: false, precent: false, output: 0, amountOfUnits: 1})
-    data.payments.push({skip: false, range: false, precent: false, output: 1, amountOfUnits: 2})
-    data.payments.push({skip: true, range: false, precent: false, output: 2, amountOfUnits: 3})
-    data.payments.push({skip: false, range: false, precent: false, output: 3, amountOfUnits: 4})
-    data.payments.push({skip: true, range: false, precent: false, output: 4, amountOfUnits: 5})
-    data.payments.push({skip: false, range: false, precent: false, output: 5, amountOfUnits: 6})
+    data.payments.push({skip: false, range: false, percent: false, output: 0, amountOfUnits: 1})
+    data.payments.push({skip: false, range: false, percent: false, output: 1, amountOfUnits: 2})
+    data.payments.push({skip: true, range: false, percent: false, output: 2, amountOfUnits: 3})
+    data.payments.push({skip: false, range: false, percent: false, output: 3, amountOfUnits: 4})
+    data.payments.push({skip: true, range: false, percent: false, output: 4, amountOfUnits: 5})
+    data.payments.push({skip: false, range: false, percent: false, output: 5, amountOfUnits: 6})
 
     result = ccEncoding.encode(data, 40)
     console.log(result.codeBuffer.toString('hex'), result.leftover)
@@ -62,7 +62,7 @@ describe('BALZ Decoding', function () {
 
     delete data.allowMeta
     data.payments = []
-    data.payments.push({skip: false, range: false, precent: true, output: 12, amountOfUnits: 3213213})
+    data.payments.push({skip: false, range: false, percent: true, output: 12, amountOfUnits: 3213213})
     result = ccEncoding.encode(data, 80)
     console.log(result.codeBuffer.toString('hex'), result.leftover)
     console.log(ccEncoding.decode(result.codeBuffer))
